25ec951a7f9b3cfd6fdd7ff724620ece905a49e7
Tobias Christiansen <tobi@tobyase.de>
2020-10-04 13:15:12 +0200

    Add prettier to the project

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,27 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint",
   },
   extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
+    "@nuxtjs",
+    "eslint:recommended",
+    "plugin:vue/recommended",
+    "plugin:prettier/recommended",
+    "plugin:nuxt/recommended",
   ],
+  plugins: ["vue"],
   // add your custom rules here
   rules: {
-  }
+    semi: [2, "never"],
+    "no-console": "off",
+    "vue/max-attributes-per-line": "off",
+    "prettier/prettier": ["error", { semi: false }],
+    "vue/html-self-closing": 0,
+    "vue/html-indent": 0,
+    "vue/singleline-html-element-content-newline": 0,
+  },
 }
