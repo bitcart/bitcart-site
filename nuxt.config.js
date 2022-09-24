114bc31088d8b5863f906fc705292121560c3353
MrNaif2018 <chuff184@gmail.com>
2022-09-25 00:48:24 +0300

    Add tron

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: "BitcartCC - non-custodial crypto payments processor",
+    title: "BitcartCC - Non-Custodial Crypto Payments Processor",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
